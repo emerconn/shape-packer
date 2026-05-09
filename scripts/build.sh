c0e9309fe846797de7019a0ac0d75899de6ecfab
Emerson Connelly <emerconn7@gmail.com>
2026-05-09 01:32:03 -0500

    remove unused ARM features

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,11 @@ MODE="${3:?}"
 OUTPUT="${4:-shape-packer}"
 
 if [ "$MODE" = "slim" ]; then
-  CGO_ENABLED=0 GOAMD64=v3 GOARM64=v8.0,lse,crypto \
+  CGO_ENABLED=0 GOAMD64=v3 GOARM64=v8.0 \
     GOOS="$GOOS" GOARCH="$GOARCH" \
     go build -ldflags="-s -w" -trimpath -o "$OUTPUT" .
 else
-  CGO_ENABLED=0 GOAMD64=v3 GOARM64=v8.0,lse,crypto \
+  CGO_ENABLED=0 GOAMD64=v3 GOARM64=v8.0 \
     GOOS="$GOOS" GOARCH="$GOARCH" \
     go build -o "$OUTPUT" .
 fi
